018cb583c27087a3e27ff07efa13c2de86c59ed0
Kyungrul <bjoh100@gmail.com>
2019-05-13 19:04:17 +0900

    First Commit

---
+++ app/src/main/java/com/lte/lte/PictureList.java
@@ -0,0 +1,15 @@
+package com.lte.lte;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+//Show a list of pictures
+
+public class PictureList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_picture_list);
+    }
+}
